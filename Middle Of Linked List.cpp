a1a35f92be653e3df1f48ead92a6d523d9988d95
Faisal Shohag <50203997+faisal-shohag@users.noreply.github.com>
2023-06-04 23:56:50 +0600

    Create Middle Of Linked List.cpp

---
+++ Middle Of Linked List.cpp
@@ -0,0 +1,43 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data; 
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+
+Node *findMiddle(Node *head) {
+    int size = 0;
+    Node* temp = head;
+
+    while(temp!=NULL) {
+        size++;
+        temp = temp->next;
+    }
+
+    int cnt = 0;
+    while(cnt<size/2) {
+        cnt++;
+        head = head->next;
+    }
+
+    return head;
+}
